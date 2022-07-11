9707fa4aeecd6f75b65b5f679fb08b00f1fceb6a
alexandru360 <alexandru360@gmail.com>
2022-07-11 16:35:59 +0300

    3 files modified

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -3,7 +3,20 @@ import * as React from 'react';
 export default function Welcome() {
   return (
     <React.Fragment>
-      <h3>Welcome</h3>
+      <div className="page-footer" xyz="fade bottom ease-in-out delay-10">
+        <div
+          className="footer-logo xyz-nested"
+          xyz="fade left ease-in-out delay-10"
+        ></div>
+        <div
+          className="footer-right"
+          xyz="fade up stagger ease-in-out delay-10"
+        >
+          <div className="footer-item xyz-nested"></div>
+          <div className="footer-item xyz-nested"></div>
+          <div className="footer-item xyz-nested"></div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
